48e9c68bbb013ce6fcb639d898b8212eebcadfeb
Tianxiao <jtxinnocence@gmail.com>
2018-01-24 16:01:32 -0500

    commit from remote

--- script/train_nmt_attention_bpe.sh
+++ script/train_nmt_attention_bpe.sh
@@ -3,16 +3,15 @@ export PYTHONPATH="${PYTHONPATH}:$(pwd)/nmt"
 python -m nmt.nmt\
     --attention=scaled_luong \
     --src=complex --tgt=simple \
-    --share_vocab=1 \
     --subword_option=bpe \
-    --vocab_prefix=model/vocab.all.tok.bpe \
-    --train_prefix=model/train.tok.bpe \
-    --dev_prefix=model/val.tok.bpe \
-    --test_prefix=model/test.tok.bpe \
-    --out_dir=model/nmt_attention_bpe_3_layers \
-    --num_train_steps=20000 \
+    --vocab_prefix=data/PWKP/vocab.tok.bpe \
+    --train_prefix=data/PWKP/train.tok.bpe \
+    --dev_prefix=data/PWKP/val.tok.bpe \
+    --test_prefix=data/PWKP/test.tok.bpe \
+    --out_dir=model/nmt_attention_bpe_4_layers \
+    --num_train_steps=50000 \
     --steps_per_stats=100 \
-    --num_layers=3 \
-    --num_units=128 \
+    --num_layers=4 \
+    --num_units=256\
     --dropout=0.2 \
     --metrics=bleu
