68393e3ae2cd88644db538aa83713b2e62cdece1
paperspace <jtxinnocence@gmail.com>
2018-01-25 12:15:00 -0500

    for merge

--- script/train_nmt_attention_glove.sh
+++ script/train_nmt_attention_glove.sh
@@ -3,9 +3,9 @@ export PYTHONPATH="${PYTHONPATH}:$(pwd)/nmt"
 python -m nmt.nmt\
     --attention=scaled_luong \
     --src=complex --tgt=simple \
-    --vocab_prefix=data/PWKP/vocab  \
-    --train_prefix=data/PWKP/train \
-    --embed_prefix=model/GloVe/glove.6B.300d\
+    --vocab_prefix=model/GloVe/glove.6B.300d.50k\
+    --train_prefix=data/WikNet/good_partial.train.tok\
+    --embed_prefix=model/GloVe/glove.6B.300d.50k\
     --dev_prefix=data/PWKP/val\
     --test_prefix=data/PWKP/test\
     --out_dir=model/nmt_attention_glove \
